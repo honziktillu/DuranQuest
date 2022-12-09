ec820dd5c3a137032c1297d801fd5bf757de4bf1
honziktillu <till@spsmb.cz>
2022-12-09 12:00:07 +0100

    Patek 02122022

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.company"},{"l":"com.company.days"},{"l":"com.company.entities"},{"l":"com.company.items"},{"l":"com.company.map"}];updateSearchResults();
